1d43f59ee25811e3cb3bf76b51dc00bf5bd21fef
Soyoon-Choi <sy.choi@altibase.com>
2024-09-24 15:04:06 +0900

    Revert "expanded page contents"
    This reverts commit a2233c91cc11072e06197d6d77f6a62a1bfaa6fe.

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.getElementById('nav-toggle');
     const nav = document.getElementById('custom-nav');
     const toggleArrow = document.getElementById('toggle-arrow');
-    const content = document.getElementById('page-content');  // 콘텐츠 영역 선택
   
     let isNavVisible = true;
   
@@ -14,12 +13,10 @@ document.addEventListener('DOMContentLoaded', function () {
         nav.classList.add('slide-in');
         nav.classList.remove('slide-out');
         toggleArrow.textContent = '←';  // Left arrow when sidebar is visible
-        content.classList.remove('expanded');  // 콘텐츠 축소
       } else {
         nav.classList.remove('slide-in');
         nav.classList.add('slide-out');
         toggleArrow.textContent = '→';  // Right arrow when sidebar is hidden
-        content.classList.add('expanded');  // 콘텐츠 확장
       }
     });
   });
