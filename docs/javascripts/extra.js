24fcd9e947b352b55c17af020a7e4be4da334983
Soyoon-Choi <sy.choi@altibase.com>
2024-09-24 14:27:22 +0900

    nav sidebar collapsible

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navToggle = document.getElementById('custom-nav-toggle');
+    const navToggle = document.getElementById('nav-toggle');
     const nav = document.getElementById('custom-nav');
-
+    const toggleArrow = document.getElementById('toggle-arrow');
+  
+    let isNavVisible = true;
+  
     navToggle.addEventListener('click', function () {
-      if (nav.style.display === 'none') {
-        nav.style.display = 'block';
+      isNavVisible = !isNavVisible;
+  
+      if (isNavVisible) {
+        nav.classList.remove('hidden');
+        nav.classList.add('slide-in');
+        nav.classList.remove('slide-out');
+        toggleArrow.textContent = '←';  // Left arrow when sidebar is visible
       } else {
-        nav.style.display = 'none';
+        nav.classList.remove('slide-in');
+        nav.classList.add('slide-out');
+        toggleArrow.textContent = '→';  // Right arrow when sidebar is hidden
       }
     });
-});
+  });
