2c3c26e08c381c8e3e3599ebd71485b7079cf607
Soyoon-Choi <sy.choi@altibase.com>
2024-09-26 07:25:04 +0900

    네비게이션 사이드 바 숨기기

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,22 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navToggle = document.getElementById('nav-toggle');
-    const nav = document.getElementById('custom-nav');
-    const toggleArrow = document.getElementById('toggle-arrow');
-  
-    let isNavVisible = true;
-  
-    navToggle.addEventListener('click', function () {
-      isNavVisible = !isNavVisible;
-  
-      if (isNavVisible) {
-        nav.classList.remove('hidden');
-        nav.classList.add('slide-in');
-        nav.classList.remove('slide-out');
-        toggleArrow.textContent = '←';  // Left arrow when sidebar is visible
-      } else {
-        nav.classList.remove('slide-in');
-        nav.classList.add('slide-out');
-        toggleArrow.textContent = '→';  // Right arrow when sidebar is hidden
-      }
-    });
+  const navToggle = document.getElementById('nav-toggle');
+  const nav = document.getElementById('custom-nav');
+  const toggleArrow = document.getElementById('toggle-arrow');
+
+  let isNavVisible = true;
+
+  navToggle.addEventListener('click', function () {
+    isNavVisible = !isNavVisible;
+
+    if (isNavVisible) {
+      nav.classList.remove('hidden');
+      nav.classList.add('slide-in');
+      nav.classList.remove('slide-out');
+      toggleArrow.textContent = '«';  // Left arrow when sidebar is visible
+      navToggle.style.left = '90%';  // Move button to right when visible
+    } else {
+      nav.classList.remove('slide-in');
+      nav.classList.add('slide-out');
+      toggleArrow.textContent = '»';  // Right arrow when sidebar is hidden
+      navToggle.style.left = '0';  // Move button to left when hidden
+    }
   });
+});
